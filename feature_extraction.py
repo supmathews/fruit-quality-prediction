a9dfb3045b91216375b54542c1e60e0ff9809157
supmathews <mathiusz@outlook.com>
2023-03-04 14:51:32 +0000

    Modified how data was flattened and cleaned the code

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,50 +1,49 @@
 #-----------Libraries for feature extraction----------#
 import os
 import numpy as np
-from PIL import Image
-import pickle
-import tensorflow as tf
-from tensorflow.keras import preprocessing
-from tensorflow.keras import layers
 from skimage.io import imread
 from skimage.transform import resize
 from sklearn.model_selection import train_test_split
 
-target = []
-images = []
-flat_data = []
+'''
+This user defined class contains method flatten_data which allows 
+the user to flatten the data in order to get the pixel values of the images
+in the form of arrays
+'''
 
-#-----------Load the images----------#
-data_dir = 'data'
-batch_size = 64
-img_height = 224
-img_width = 224
+class FlattenData:
 
-CATEGORIES = ['no_split', 'split']
+    def __init__(self, data_dir, batch_size=64, img_height=224, img_width=224):
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.img_height = img_height
+        self.img_width = img_width
 
-for category in CATEGORIES:
-    class_num = CATEGORIES.index(category)
-    path = os.path.join(data_dir, category)
-    for img in os.listdir(path):
-        img_array = imread(os.path.join(path, img))
-        img_resized = resize(img_array, (img_height, img_width, 3))
-        flat_data.append(img_resized.flatten())
-        images.append(img_resized)
-        target.append(class_num)
 
-flat_data = np.array(flat_data)
-target = np.array(target)
-images = np.array(images)
+    # Function to flatten the data
+    def flatten_data(self):
+        
+        target = []
+        flat_data = []
 
-# Split the data into training and testing data
-x_train, x_test, y_train, y_test = train_test_split(flat_data, target, test_size=0.3, random_state=1234)
-print(x_train.shape)
-print(x_test.shape)
-print(y_train.shape)
-print(y_test.shape)
+        CATEGORIES = ['no_split', 'split']
+        
+        for category in CATEGORIES:
+            class_num = CATEGORIES.index(category)
+            path = os.path.join(self.data_dir, category)
+            
+            for img in os.listdir(path):
+                img_array = imread(os.path.join(path, img))
+                img_resized = resize(img_array, (self.img_height, self.img_width, 3))
+                flat_data.append(img_resized.flatten())
+                target.append(class_num)
+        
+        # Convert the data to np.array format
+        flat_data = np.array(flat_data)
+        target = np.array(target)
 
-train_test_files = [x_train, x_test, y_train, y_test]
+        # Split the data into training and testing data
+        x_train, x_test, y_train, y_test = train_test_split(flat_data, target, test_size=0.3, random_state=1234)
 
-# Save the files
-with open('train_test_files.pkl', 'wb') as f:
-    pickle.dump(train_test_files, f)
+        # Retrun the splits
+        return x_train, x_test, y_train, y_test
