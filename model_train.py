65fe634b4139382432cb5726d7c1b8ce0f50ab89
supmathews <mathiusz@outlook.com>
2023-03-04 15:28:24 +0000

    Added comments and removed unused dependencies

--- model_train.py
+++ model_train.py
@@ -1,25 +1,21 @@
 #-----------Libraries for feature extraction----------#
-import os
-import numpy as np
-from PIL import Image
 import pickle
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow.keras import preprocessing
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
 from sklearn import svm
 from feature_extraction import FlattenData
 
+
+# Function train the model using grid search and dump the model as pkl file
 def get_model(x_train, y_train, x_test):
+
+    # Parameter grid to search through
     param_grid = [
         {'C':[1, 10, 100, 1000], 'kernel':['linear', 'sigmoid']},
         {'C':[1, 10, 100, 1000], 'gamma':[0.001, 0.0001, 'auto'] ,'kernel':['linear', 'rbf', 'poly']}
     ]
-
+    
+    # Call the SVM model and using grid search to find best hyperparameters
     model = svm.SVC()
     clf = GridSearchCV(model, param_grid=param_grid, verbose=1)
     clf.fit(x_train, y_train)
@@ -39,4 +35,5 @@ if __name__ == '__main__':
     train_test_data = FlattenData(data_dir='data')
     x_train, x_test, y_train, y_test = train_test_data.flatten_data()
 
+    # Executing the user defined function from above
     get_model(x_train, y_train, x_test)
