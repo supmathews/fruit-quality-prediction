8c89231943f101bd1affea9d254d09da070ad488
supmathews <mathiusz@outlook.com>
2023-03-04 15:29:18 +0000

    Added comments and removed unused dependencies

--- model_prediction.py
+++ model_prediction.py
@@ -1,54 +1,19 @@
-#-----------Libraries for feature extraction----------#
-import os
-import numpy as np
-from numpy.linalg import norm
-from PIL import Image
+#-----------Libraries----------#
 import pickle
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow.keras import preprocessing
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
-from sklearn.metrics import accuracy_score
-from skimage.io import imread
-from skimage.transform import resize
+from feature_extraction import FlattenData
 
 CATEGORIES = ['no_split', 'split']
 
-def save_uploaded_file(uploaded_file):
-  try:
-      with open(os.path.join('uploads', uploaded_file.name), 'wb') as f:
-        f.write(uploaded_file.getbuffer())
-      return 1
-  except:
-      return 0
-  
-def extract_features(img_path):
-    flat_data = []
-    #url = input('Enter image url : ')
-    img = imread(img_path)
-    img_resized = resize(img, (224, 224, 3))
-    flat_data.append(img_resized.flatten())
-    flat_data = np.array(flat_data)
-    return flat_data
-
 if __name__ == '__main__':
 
-    flat_data = []
-    url = input('Enter image url : ')
-    img = imread(url)
-    img_resized = resize(img, (224, 224, 3))
-    flat_data.append(img_resized.flatten())
-    flat_data = np.array(flat_data)
-    
-    print(img.shape)
-    
-    plt.imshow(img_resized)
-    
-    # Call the CNN model
+    # Retrieve the flattened data
+    data = FlattenData()
+    flat_data = data.flatten_image()
+        
+    # Call the SVM model
     model = pickle.load(open('model.pkl', 'rb'))
 
+    # Make the prediction
     y_out = model.predict(flat_data)
     y_out = CATEGORIES[y_out[0]]
     print(f'>Predicted output : {y_out}')
